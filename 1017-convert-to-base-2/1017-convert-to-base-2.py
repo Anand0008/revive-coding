b6a05b98d07b1770e7f487605198becf3366a234
Anand0008 <61839281+Anand0008@users.noreply.github.com>
2025-11-10 09:06:51 +0530

    Correct base -2 conversion logic
    Fix integer division and remainder calculation for negative base conversion.

--- 1017-convert-to-base-2/1017-convert-to-base-2.py
+++ 1017-convert-to-base-2/1017-convert-to-base-2.py
@@ -4,8 +4,8 @@ class Solution:
         base = -2
         ans = ""
         while(n!=0):
-            rem = n % base
-            n = n // base
+            rem = n % base 
+            n = n // base  #dont use n = int(n/base)  5 // -2 = -3   (integer floor division) 5 / -2 = -2.5 int(-2.5) = -2   "// in Python rounds down toward −∞ but int() truncates toward 0"
             if (rem < 0):
                 rem += 2
                 n += 1
