aa288a0ad7fb4e5e1e0dd4f063c4d8ad99201708
Anand0008 <61839281+Anand0008@users.noreply.github.com>
2025-11-25 12:54:29 +0530

    Time: 0 ms (100%), Space: 7.8 MB (82.38%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,16 +1,11 @@
 #include<cmath>
 class Solution {
 public:
-    bool is_integer(double ans){
-        return std::floor(ans) == ans;
-    }
+    
 
     bool isPowerOfTwo(int n) {
         if (n<=0) return false;
-        if (n==1 || n==2) return true;
-        double ans = log2(n);
-        if (is_integer(ans)) return true;
-        else return false;
+        return ((n&(n-1))==0);
         
     }
 };
