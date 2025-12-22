b675b85fe2931b33327698241203f95043c5b162
Anand0008 <61839281+Anand0008@users.noreply.github.com>
2025-12-22 15:30:42 +0530

    Delete 0125-valid-palindrome/0125-valid-palindrome-two-pointer.py

--- 0125-valid-palindrome/0125-valid-palindrome-two-pointer.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        filtered = []
-
-        for c in s:
-            if c.isalnum():
-                filtered.append(c.lower())
-
-        return filtered == filtered[::-1]
